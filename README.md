29d0ed09b8ddca94aafe87cfbce7721922757424
David Reid <dreid@dreid.org>
2012-03-10 09:54:35 -0800

    simplejson and README

---
+++ README.md
@@ -0,0 +1,3 @@
+## stimpy: STatebox In My PYthon
+
+stimpy is a python implementation of https://github.com/mochi/statebox/
--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="0.0.1",
     description="STatebox In My PYthon",
     py_modules=["stimpy"],
-    install_requires=["Twisted", "mock"]
+    install_requires=["Twisted", "mock", "simplejson"]
 )
