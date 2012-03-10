c201ea8edfb96c06c1fba3b75f503f96eee28bda
David Reid <dreid@dreid.org>
2012-03-10 09:51:38 -0800

    Build on travis

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="stimpy",
+    version="0.0.1",
+    description="STatebox In My PYthon",
+    py_modules=["stimpy"],
+    install_requires=["Twisted", "mock"]
+)
